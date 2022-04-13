d504e5e9e35ebd84e25b92ee7a0333df8846e78d
laird-ikar <bguyot@student.42mulhouse.fr>
2022-04-13 08:45:03 +0200

    simplified cd (not var interpretor in builtin)

--- src/builtin/echo.c
+++ src/builtin/echo.c
@@ -6,15 +6,13 @@
 /*   By: bguyot <bguyot@student.42mulhouse.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/11 09:40:00 by bguyot            #+#    #+#             */
-/*   Updated: 2022/04/11 10:23:05 by bguyot           ###   ########.fr       */
+/*   Updated: 2022/04/13 08:21:40 by bguyot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/builtin.h"
 
-void	ft_print_split(char **split, t_mshell mshell);
-
-void	ft_echo(char **args, t_mshell *mshell)
+int	ft_echo(char **args, t_mshell *mshell)
 {
 	char	**split;
 	char	**ptr;
@@ -23,33 +21,19 @@ void	ft_echo(char **args, t_mshell *mshell)
 	{
 		split = ft_split(*args, ' ');
 		ptr = split;
-		while (*(split++))
+		while (*(ptr++))
 		{
-			ft_print_split(split, mshell);
+			ft_putstr_fd(*ptr, 1);
+			ft_putchar_fd(' ', 1);
 		}
+		ptr = split;
 		while (*ptr)
 		{
 			free(*ptr++);
 		}
-		free(ptr);
+		free(split);
 	}
-}
-
-void	ft_print_split(char **split, t_mshell mshell)
-{
-	if (!ft_strchr(*split, '$'))
-		ft_putstr_fd(*split, 0);
-	else
-	{
-		while (*((*split)++))
-		{
-			if (**split == '$')
-			{
-				ft_putstr_fd(ft_getenv(mshell, (*split)++), 0);
-				break ;
-			}
-			ft_putchar_fd(**split, 0);
-		}
-	}
-	ft_putchar_fd(' ', 0);
+	ft_putchar_fd('\n', 1);
+	return (0);
+	(void) mshell;
 }
