266b00c673bdae651bac504ad6f027560902cc14
laird-ikar <bguyot@student.42mulhouse.fr>
2022-04-13 08:44:12 +0200

    cd return value

--- src/builtin/cd.c
+++ src/builtin/cd.c
@@ -6,7 +6,7 @@
 /*   By: bguyot <bguyot@student.42mulhouse.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/11 09:19:29 by bguyot            #+#    #+#             */
-/*   Updated: 2022/04/11 10:03:47 by bguyot           ###   ########.fr       */
+/*   Updated: 2022/04/13 08:21:10 by bguyot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 static int	use_path(char *path, t_mshell *mshell);
 
-void	ft_cd(char **args, t_mshell *mshell)
+int	ft_cd(char **args, t_mshell *mshell)
 {
 	char	*tmp;
 
@@ -22,18 +22,19 @@ void	ft_cd(char **args, t_mshell *mshell)
 	if (args[1])
 	{
 		if (use_path(args[1], mshell))
-			return ;
+			return (1);
 	}
 	else
 	{
 		if (chdir(ft_getenv(mshell, "HOME")))
 		{
 			printf("Error : bad home");
-			return ;
+			return (1);
 		}
 	}
-	ft_setenv(mshell, "OLDPWD", tmp);
-	ft_setenv(mshell, "PWD", getcwd(NULL, 0));
+	ft_setenv(mshell, "OLDPWD", tmp, EXPORTED);
+	ft_setenv(mshell, "PWD", getcwd(NULL, 0), EXPORTED);
+	return (0);
 }
 
 static int	use_path(char *path, t_mshell *mshell)
